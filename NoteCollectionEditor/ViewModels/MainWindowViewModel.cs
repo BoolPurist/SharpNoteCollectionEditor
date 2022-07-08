3020ca63fcb1f78b902e8f0e5ab5d36e1f2a1bcf
BoolPurist <fnxa@posteo.de>
2022-07-08 02:08:48 +0200

    Init: created project.

---
+++ NoteCollectionEditor/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace NoteCollectionEditor.ViewModels
+{
+  public class MainWindowViewModel : ViewModelBase
+  {
+    public string Greeting => "Welcome to Avalonia!";
+  }
+}
---
+++ NoteCollectionEditor/Views/MainWindow.axaml.cs
@@ -0,0 +1,12 @@
+using Avalonia.Controls;
+
+namespace NoteCollectionEditor.Views
+{
+  public partial class MainWindow : Window
+  {
+    public MainWindow()
+    {
+      InitializeComponent();
+    }
+  }
+}
