ac38156845035c1f49d1d844ddaa3b63ca097c90
BoolPurist <fnaumann@posteo.de>
2022-11-05 23:59:08 +0100

    DevSetupFix: path to saved file is now at correct dev path.

--- NoteCollectionEditor/ConfigMapping/AppConfigs.cs
+++ NoteCollectionEditor/ConfigMapping/AppConfigs.cs
@@ -8,74 +8,87 @@ namespace NoteCollectionEditor.ConfigMapping;
 
 public class AppConfigs : IAppConfigs
 {
-  private const string SectionNameForPathToNoteListSource = "NoteListDataName";
+    private const string SectionNameForPathToNoteListSource = "NoteListDataName";
 
-  public AppDevelopmentConfig DevelopmentConfiguration { get; private set; } = new();
-  public string PathToNoteSource { get; private set; } = String.Empty;
+    public AppDevelopmentConfig DevelopmentConfiguration { get; private set; } = new();
+    public string PathToNoteSource { get; private set; } = String.Empty;
 
-  public string AppVersion { get; set; } = String.Empty;
+    public string AppVersion { get; set; } = String.Empty;
 
-  public string AppLink { get; set; } = String.Empty;
+    public string AppLink { get; set; } = String.Empty;
 
 
 
-  public static AppConfigs CreateWithoutConfigFile()
-  {
-    return new AppConfigs();
-  }
+    public static AppConfigs CreateWithoutConfigFile()
+    {
+        return new AppConfigs();
+    }
 
-  public static AppConfigs Create()
-  {
-    var appConfig = new AppConfigs();
+    public static AppConfigs Create()
+    {
+        var appConfig = new AppConfigs();
 
-    var config = new ConfigurationBuilder()
-      .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
-      .AddJsonFile("appsettings.json")
+        var config = new ConfigurationBuilder()
+          .SetBasePath(AppDomain.CurrentDomain.BaseDirectory)
+          .AddJsonFile("appsettings.json")
 #if DEBUG
-      .AddJsonFile("appsettings.develop.json")
+          .AddJsonFile("appsettings.develop.json")
 #endif
-      .Build();
+          .Build();
 
-    if (EnvironmentUtils.IsInDevelopment())
-    {
-      appConfig.DevelopmentConfiguration = GetSectionAsBindingByType<AppDevelopmentConfig>(config);
+        if (EnvironmentUtils.IsInDevelopment())
+        {
+            appConfig.DevelopmentConfiguration = GetSectionAsBindingByType<AppDevelopmentConfig>(config);
+        }
+
+        var nameJsonDataFile = GetSectionAsBinding<string>(config, SectionNameForPathToNoteListSource);
+        appConfig.PathToNoteSource = Environment.CurrentDirectory;
+
+        ApplyDevelopmentSettings(appConfig, config);
+
+        appConfig.PathToNoteSource = Path.Join(appConfig.PathToNoteSource, nameJsonDataFile);
+
+        Console.WriteLine($"appConfig.PathToNoteSource: {appConfig.PathToNoteSource}");
+        appConfig.AppVersion = GetSectionAsBinding<string>(config, nameof(AppVersion));
+        appConfig.AppLink = GetSectionAsBinding<string>(config, nameof(AppLink));
+
+        return appConfig;
     }
 
-    appConfig.PathToNoteSource = GetSectionAsBinding<string>(config, SectionNameForPathToNoteListSource);
-    appConfig.PathToNoteSource = Path.Join(Environment.CurrentDirectory, appConfig.PathToNoteSource);
-    appConfig.AppVersion = GetSectionAsBinding<string>(config, nameof(AppVersion));
-    appConfig.AppLink = GetSectionAsBinding<string>(config, nameof(AppLink));
-
-
-    return appConfig;
-  }
-
-  private static T GetSectionAsBindingByType<T>(IConfigurationRoot root)
-  {
-    var boundSection = root.GetSection(typeof(T).Name).Get<T>();
-    Debug.Assert(boundSection != null);
-    return boundSection;
-  }
-
-  private static T GetSectionAsBinding<T>(IConfigurationRoot root, string nameSection)
-  {
-    var boundSection = root.GetSection(nameSection).Get<T>();
-    Debug.Assert(boundSection != null);
-    return boundSection;
-  }
-
-  public string GetDefaultPathExportImport()
-  {
-    if (EnvironmentUtils.IsInDevelopment())
+    private static void ApplyDevelopmentSettings(AppConfigs appConfig, IConfigurationRoot config)
     {
-      var projectPath = ProjectSourcePath.GetPathToProject();
-      var relativeExportPath = DevelopmentConfiguration.PathToDataDump;
-      var exportPath = Path.Join(projectPath, relativeExportPath);
-      return exportPath;
+#if DEBUG
+        var appDevConfig = GetSectionAsBindingByType<AppDevelopmentConfig>(config);
+        appConfig.PathToNoteSource = Path.Join(appConfig.PathToNoteSource, appDevConfig.PathToDataDump);
+#endif
     }
 
-    return Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
-  }
+    private static T GetSectionAsBindingByType<T>(IConfigurationRoot root)
+    {
+        var boundSection = root.GetSection(typeof(T).Name).Get<T>();
+        Debug.Assert(boundSection != null);
+        return boundSection;
+    }
+
+    private static T GetSectionAsBinding<T>(IConfigurationRoot root, string nameSection)
+    {
+        var boundSection = root.GetSection(nameSection).Get<T>();
+        Debug.Assert(boundSection != null);
+        return boundSection;
+    }
+
+    public string GetDefaultPathExportImport()
+    {
+        if (EnvironmentUtils.IsInDevelopment())
+        {
+            var projectPath = ProjectSourcePath.GetPathToProject();
+            var relativeExportPath = DevelopmentConfiguration.PathToDataDump;
+            var exportPath = Path.Join(projectPath, relativeExportPath);
+            return exportPath;
+        }
+
+        return Environment.GetFolderPath(Environment.SpecialFolder.UserProfile);
+    }
 
 
 
